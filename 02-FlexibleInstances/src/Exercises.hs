501e5f651b754ac8073c6b67dac9df595b09f556
micheleberetta98 <beretta.m98@gmail.com>
2022-11-17 14:27:52 +0100

    02-FlexibleInstances

--- 02-FlexibleInstances/src/Exercises.hs
+++ 02-FlexibleInstances/src/Exercises.hs
@@ -5,30 +5,30 @@ class PopQuiz a
 -- | Which of the following instances require 'FlexibleInstances'? Don't cheat
 -- :D This is a tricky one, but look out for nested concrete types!
 
--- instance PopQuiz Bool
--- instance PopQuiz [Bool]
--- instance PopQuiz [a]
--- instance PopQuiz (a, b)
--- instance PopQuiz [(a, b)]
--- instance PopQuiz (IO a)
+-- instance PopQuiz Bool            -- ok
+-- instance PopQuiz [Bool]          -- needs extension
+-- instance PopQuiz [a]             -- ok
+-- instance PopQuiz (a, b)          -- ok
+-- instance PopQuiz [(a, b)]        -- needs extension
+-- instance PopQuiz (IO a)          -- ok
 
 newtype RIO  r a = RIO (r -> IO a) -- Remember, this is a /new type/.
 type    RIO' r a =      r -> IO a
 
--- instance PopQuiz (RIO Int a)
--- instance PopQuiz (RIO r a)
--- instance PopQuiz (RIO' r a)
--- instance PopQuiz (r -> IO a)
--- instance PopQuiz (a -> b) -- We can write (a -> b) as ((->) a b).
--- instance PopQuiz (a -> b -> c)
--- instance PopQuiz (a, b, c)
--- instance PopQuiz (a, (b, c))
--- instance PopQuiz ()
--- instance PopQuiz (a, b, c, a)
+-- instance PopQuiz (RIO Int a)     -- needs extension
+-- instance PopQuiz (RIO r a)       -- ok
+-- instance PopQuiz (RIO' r a)      -- needs extension
+-- instance PopQuiz (r -> IO a)     -- needs extension
+-- instance PopQuiz (a -> b)        -- ok
+-- instance PopQuiz (a -> b -> c)   -- needs extension
+-- instance PopQuiz (a, b, c)       -- ok
+-- instance PopQuiz (a, (b, c))     -- needs extension
+-- instance PopQuiz ()              -- ok
+-- instance PopQuiz (a, b, c, a)    -- needs extension
 
 data Pair  a = Pair  a  a
 type Pair' a =      (a, a)
 
--- instance PopQuiz (a, a)
--- instance PopQuiz (Pair a)
--- instance PopQuiz (Pair' a)
+-- instance PopQuiz (a, a)          -- needs extension
+-- instance PopQuiz (Pair a)        -- ok
+-- instance PopQuiz (Pair' a)       -- needs extension
